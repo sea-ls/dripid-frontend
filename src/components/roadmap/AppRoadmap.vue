49d74f79fdb2194dcc17a28d102b6386bfac51a4
ifursenko <ifursenko2001@gmail.com>
2024-08-01 18:28:00 +0400

    fixes

--- src/components/roadmap/AppRoadmap.vue
+++ src/components/roadmap/AppRoadmap.vue
@@ -2,9 +2,11 @@
   <div>
     <div class="d-flex flex-column justify-center" v-if="mobile">
       <h2 class="roadmap-title">Как заказать доставку?</h2>
-      <v-switch class="justify-center d-flex" color="#304FFE" inset v-model="selected"  :label="selected ? 'У меня есть зарубежная карта' : 'У меня нет зарубежной карты'"></v-switch>
+      <v-switch class="justify-center d-flex" color="#304FFE" inset v-model="selected"
+                :label="selected ? 'У меня есть зарубежная карта' : 'У меня нет зарубежной карты'"></v-switch>
       <div class="d-flex justify-center ga-10">
-        <div v-if="selected" class="roadmap d-flex justify-space-between align-center  ga-2 flex-column" @click="selected = false">
+        <div v-if="selected" class="roadmap d-flex justify-space-between align-center  ga-2 flex-column"
+             @click="selected = false">
 
           <v-card width="300px" height="200px" rounded="xl" class="bg-grey-lighten-3">
             <v-card-title class="w-50 bg-indigo-accent-4 card-title">ШАГ 2</v-card-title>
@@ -21,7 +23,8 @@
             </v-card-text>
           </v-card>
         </div>
-        <div  v-else class="roadmap d-flex justify-space-between align-center  ga-2 flex-column" @click="selected = true">
+        <div v-else class="roadmap d-flex justify-space-between align-center  ga-2 flex-column"
+             @click="selected = true">
           <v-card width="300px" height="200px" rounded="xl" class="bg-indigo-accent-4">
             <v-card-title class="bg-grey-lighten-3 w-50 card-title">ШАГ 1</v-card-title>
             <v-card-text>
@@ -56,7 +59,10 @@
     <div class="d-flex flex-column justify-center" v-else>
       <h2 class="roadmap-title">Как заказать доставку?</h2>
       <div class="d-flex justify-center ga-10">
-        <div  :class="selected ? 'blur' : ''" class="roadmap d-flex justify-space-between align-center  ga-2 flex-column" @click="selected = false">
+        <div>
+          <p class="text-h5">У меня есть зарубежная карта</p>
+          <div :class="selected ? 'blur' : ''" class="roadmap d-flex justify-space-between align-center  ga-2 flex-column"
+               @click="selected = false">
             <RoadmapCard
                 v-for="(card, i) in withCardRoadmapData"
                 :key="card.subtitle"
@@ -66,8 +72,25 @@
             ></RoadmapCard>
           </div>
         </div>
+
+        <div>
+          <p class="text-h5">У меня нет зарубежной карты</p>
+          <div :class="selected ? '' : 'blur'" class="roadmap d-flex justify-space-between align-center  ga-2 flex-column"
+               @click="selected = true">
+            <RoadmapCard
+                v-for="(card, i) in withoutCardRoadmap"
+                :key="card.subtitle"
+                :subtitle="card.subtitle"
+                :text="card.text"
+                :index="++i"
+            ></RoadmapCard>
+          </div>
+        </div>
+
+
       </div>
     </div>
+  </div>
 
 </template>
 
@@ -100,16 +123,43 @@ export default {
       },
     ]
 
+    const withoutCardRoadmap = [
+      {
+        subtitle: 'Регистрация',
+        text: 'Зарегестрируйтесь на сервисе DRIPID и получите персональный адрес'
+      },
+      {
+        title: 'ШАГ 2',
+        subtitle: 'Покупка',
+        text: 'Покупайте в Интернет-магазинах США, укажите свой персональный адрес при покупке'
+      },
+      {
+        title: 'ШАГ 3',
+        subtitle: 'Доставка',
+        text: 'Упаковка и быстрое получение из любой точки мира'
+      },
+      {
+        title: 'ШАГ 4',
+        subtitle: 'Доставка',
+        text: 'Упаковка и быстрое получение из любой точки мира'
+      },
+    ]
+
     return {
       selected,
       mobile,
-      withCardRoadmapData
+      withCardRoadmapData,
+      withoutCardRoadmap
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+
+.blur {
+  filter: blur(20px);
+}
 
 
 .roadmap__block {
