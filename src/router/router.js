b260dcc1b65def555d5feff69efcc007a315bb9b
Alexey Tyurin <alexsei89tyrin@gmail.com>
2024-08-10 21:07:05 +0400

    fix router history

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import MainLayout from "@/layouts/MainLayout.vue";
 import MainPage from "../pages/MainPage.vue";
 import PersonalAreaPage from "../pages/PersonalAreaPage.vue";
@@ -61,7 +61,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes,
 })
 
