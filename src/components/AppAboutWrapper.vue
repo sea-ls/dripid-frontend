6ca214985ceb6add478995dd4fd9d41705234890
Alexey Tyurin <alexsei89tyrin@gmail.com>
2024-08-10 21:12:03 +0400

    about block redesign

--- src/components/AppAboutWrapper.vue
+++ src/components/AppAboutWrapper.vue
@@ -2,13 +2,11 @@
   <!--   <div class="about bg-indigo-accent-4 align-center d-flex flex-column position-relative ">-->
   <!--     <h2 class='about-header'>О НАС</h2>-->
   <!--   </div>-->
-  <v-card class="about bg-indigo-accent-4 align-center d-flex flex-column position-relative w-100 h-auto">
+  <v-card class="about bg-indigo-accent-4 d-flex flex-column align-center justify-center position-relative w-100">
     <v-card-title>
       <h2 class='about-header'>О НАС</h2>
     </v-card-title>
-    <v-card-text>
       <p class="about-text text-body-1 text-md-h4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus earum fugiat odit porro quos temporibus voluptas. Aliquam cumque doloremque earum ipsam officiis qui saepe similique tenetur, ut voluptatum. Reiciendis, veniam.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur aut blanditiis consequuntur culpa dolor eligendi, eos ex in iusto laborum officiis quaerat quisquam reiciendis rem repellendus repudiandae veritatis vero voluptatum.</p>
-    </v-card-text>
   </v-card>
 </template>
 
@@ -23,7 +21,8 @@ export default {
   box-sizing: content-box;
   padding: 1rem 0;
   border-radius: 30px;
-  height: 50vh;
+  height: 80vh;
+  padding: 40px;
 }
 
 .about-header {
